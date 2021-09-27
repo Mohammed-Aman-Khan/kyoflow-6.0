5195c989fd6de73039576a1134c66a94d82df6ec
Mohammed-Aman-Khan <45879026+Mohammed-Aman-Khan@users.noreply.github.com>
2021-09-28 00:04:07 +0530

    Update

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,5 +1,42 @@
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexGrow: 1,
+        height: '100vh',
+    },
+    leftPanel: {
+        backgroundColor: '#E0E0E0',
+        height: '100%',
+        width: '15%',
+    },
+    centerPanel: {
+        height: '100%',
+        width: '70%',
+    },
+    rightPanel: {
+        backgroundColor: '#E0E0E0',
+        height: '100%',
+        width: '15%',
+    },
+})
+
 const UI = () => {
-    return <></>
+    const classes = useStyles()
+
+    return <div className={ classes.container }>
+        <div className={ classes.leftPanel }>
+
+        </div>
+        <div className={ classes.centerPanel }>
+
+        </div>
+        <div className={ classes.rightPanel }>
+
+        </div>
+    </div>
 }
 
 export default UI
