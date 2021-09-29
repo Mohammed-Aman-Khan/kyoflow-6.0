fa0f74c1a37dc06421cc04d247c328cb7e796c8f
Xavier-Xo <70265592+Xa-vie@users.noreply.github.com>
2021-09-29 20:00:17 +0530

    State

--- src/components/State/index.js
+++ src/components/State/index.js
@@ -1,5 +1,81 @@
+import { useEffect, useState } from "react"
+
 const State = () => {
-    return <></>
+
+
+    const initialState = (parameter) => {
+
+        switch (parameter) {
+            case "bool":
+                return false
+            case "number":
+                return 0
+            case "string":
+                return ''
+            case "object":
+                return {}
+            case "array":
+                return []
+            default: return null;
+        }
+
+
+    }
+
+    // const data
+
+
+
+
+    const [state, setState] = useState() // created magic state need 
+
+    const dataType = (newFruit) => {
+        setState(state => initialState(newFruit))
+    }
+
+    const setValue = (data) => {
+        setState(data)
+    }
+
+
+    useEffect(() => {
+        console.log('state:', state)
+    }, [state])
+
+
+
+
+
+
+    return <>
+        <form>
+            <select
+                onChange={(event) => dataType(event.target.value)}
+            >
+                <option value="array">array</option>
+                <option value="bool">bool</option>
+                <option value="string">string</option>
+                <option value="object">object</option>
+                <option value="number">number</option>
+            </select>
+        </form>
+        {
+            typeof state === "boolean"
+                ?
+                <div>
+                    <input type="checkbox" defaultChecked={state} onChange={() => setState(!state)} />
+                </div>
+                :
+                <div>
+                    <label>Please specify:</label>
+                    <input value={state} onInput={e => setValue(e.target.value)} />
+                </div>
+        }
+
+
+
+        <div> your state : {state}</div>
+    </>
 }
 
 export default State
